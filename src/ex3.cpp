453b0fc395ade60d935299dbdb09902a54db3dda
nC753 <notchcry951warrior@gmail.com>
2026-04-27 19:39:31 +0500

    ex3 ready

---
+++ src/ex3.cpp
@@ -0,0 +1,20 @@
+#include "Arduino.h"
+
+#define RED_LED_PIN 26
+#define SENSOR 33
+
+int lightvalue = 0;
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(RED_LED_PIN, OUTPUT);
+    pinMode(SENSOR, INPUT);
+}
+
+void loop() {
+    digitalWrite(RED_LED_PIN, HIGH);
+    lightvalue = analogRead(SENSOR);
+    Serial.print("Light value = ");
+    Serial.print(lightvalue);
+    delay(500);
+}
