cc16a9bef97d4e542deeb13462361e4a69bff52c
nC753 <notchcry951warrior@gmail.com>
2026-04-27 18:27:48 +0500

    modification of ex2.cpp and platformio.ini

--- src/ex2.cpp
+++ src/ex2.cpp
@@ -1,13 +1,10 @@
-
-
-
 #include "Arduino.h"
 
 #define GREEN_LED_PIN 27
 #define BUTTON_PIN 25
 
 bool greenState = false;
-bool ledState = HIGH; 
+int lastButtonState = LOW;
 
 /****************************************************/
 void setup(void) 
@@ -22,14 +19,15 @@ void setup(void)
 /****************************************************/
 void loop(void) 
 {
-    bool currentButtonState = digitalRead(BUTTON_PIN);
-    
-    if (buttonState == LOW && lastButtonState == HIGH) {
+    int buttonState = digitalRead(BUTTON_PIN);
+       
+    if (buttonState == HIGH && lastButtonState == LOW) {
         greenState = !greenState;
         digitalWrite(GREEN_LED_PIN, greenState ? HIGH : LOW);
         
         Serial.println(greenState ? "GREEN=1" : "GREEN=0");
     }
 
-    delay(1000); // Wait for 1000 ms
+    lastButtonState = buttonState;
+    delay(50); // Wait for 50 ms
 }
